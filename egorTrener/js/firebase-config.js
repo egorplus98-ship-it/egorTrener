1fbafdec99784b65d1118903dbcf0b457e32f539
egorplus98-ship-it <egorplus98@gmail.com>
2026-05-05 16:17:50 +0700

    Add files via upload

---
+++ egorTrener/js/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDCsFzRcG3qpS7kL42QZwsLho8eh8EirNY",
+    authDomain: "egortrener-5485d.firebaseapp.com",
+    projectId: "egortrener-5485d",
+    storageBucket: "egortrener-5485d.firebasestorage.app",
+    messagingSenderId: "876864869628",
+    appId: "1:876864869628:web:c49c65ac028490f5cdf379"
+};
